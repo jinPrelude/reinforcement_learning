3d7f75ac1d2942663af255be8dbf878db2048d1a
jinPrelude <jin.Prelude@gmail.com>
2018-08-16 09:24:29 +0900

    Remove the comments

--- DDPG/Pendulum/agent.py
+++ DDPG/Pendulum/agent.py
@@ -13,57 +13,41 @@ class ActorNetwork(object):
         self.tau = tau
         self.batch_size = batch_size
 
-        # Actor Network를 생성.
+
         with tf.variable_scope('actor_network') :
             self.inputs, self.out, self.scaled_out = self.create_actor_network()
 
-        # It stores the parameters the network has.
-        #
+
         self.network_params = tf.trainable_variables()
 
-        # Target Actor network를 생성.
+
         with tf.variable_scope('target_actor_network') :
             self.target_inputs, self.target_out, self.target_scaled_out = self.create_actor_network()
 
-        # It stores the parameters the target network has.
-        # tf.trainable_variables 에 network_params 다음 target_network_params가 바로 이어져 있어서
-        # 슬라이싱을 해줌
+
         self.target_network_params = tf.trainable_variables()[
                                      len(self.network_params):]
 
 
-        # Op for periodically updating target network with online network
-        # weights
-        # update_target_network_params = tau*t theta[i] + (1-tau) * target_theta[i]
-        # .assign은 assign() 괄호 안의 내용대로 변수의 값을 변경해주는 함수인듯
+
         self.update_target_network_params = \
             [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) +
                                                   tf.multiply(self.target_network_params[i], 1. - self.tau)) for i in range(len(self.target_network_params))]
 
-        # critic network에게 제공받을 placeholder
-        #  action의 gradient임.
+
         self.action_gradient = tf.placeholder(tf.float32, [None, self.a_dim])
 
-        # Combine the gradients here
-        # ys인 scaled_out을 xs인 network_params에 대해서 미분하고, y_grads인 -self.action_gradient를 곱해준다.
-        # y_grads 가 ys와 같은 독립변수를 포함하는 함수이면 placeholder에 따라 변경된 y_grads와 scaled_out이 곱해져서
-        # 편하다. self.action_gradient 가 음수인 이유는 gradient ascent 가 아닌 gradient descent를 해야 하기 때문이다.
+
         self.unnormalized_actor_gradients = tf.gradients(
             self.scaled_out, self.network_params, -self.action_gradient)
 
         self.actor_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_actor_gradients))
 
-        # 최적화 부분
-        # optimization part
-        # actor_gradients 의 값을 Adam optimizer 을 이용해서 network_params에 변동사항을 적용하는 부분인듯. 그래서
-        # actor_gradients랑 network_params를 zip으로 묶어둔 듯 하다.
         self.optimize = \
             tf.train.AdamOptimizer(self.learning_rate).apply_gradients(zip(self.actor_gradients, self.network_params))
 
-        #훈련시킬 네트워크 파라메터가 몇 개인지 저장한다.
         self.num_trainable_vars = len(self.network_params) + len(self.target_network_params)
 
-    # Define actor neural network
     def create_actor_network(self):
 
         inputs = tf.placeholder(name='actor_input', shape=[None, self.s_dim], dtype=tf.float32)
@@ -82,37 +66,26 @@ class ActorNetwork(object):
         w4 = tf.get_variable(name='actor_w4', shape=[6, self.a_dim], initializer=tf.random_uniform_initializer(-0.3, 0.3))
         l4 = tf.matmul(l3, w4)
         out = tf.nn.tanh(l4)
-        # Scale output to -action_bound to action_bound
         scaled_out = tf.multiply(out, self.action_bound)
         return inputs, out, scaled_out
 
 
-    # action의 gradient 와 inputs(state)를 입력으로 받아 self.optimize를 돌려서 학습한다.
-    # Train by running self.optimize which gets the gradient of the action and inputs(state) as a inputs
     def train(self, inputs, a_gradient):
         self.sess.run(self.optimize, feed_dict={
             self.inputs: inputs,
             self.action_gradient: a_gradient
         })
 
-    # input을 받아 예측한 행동을 반환
-    # Choose and return the action of the actor network
-    # by getting input(state) as a input
     def predict(self, inputs):
         return self.sess.run(self.scaled_out, feed_dict={
             self.inputs: inputs
         })
 
-    # target network의 행동 예측값을 반환
-    # Choose and return the action of the target actor network
-    # by getting input(state) as a input
     def predict_target(self, inputs):
         return self.sess.run(self.target_scaled_out, feed_dict={
             self.target_inputs: inputs
         })
 
-    # target network를 self.update_target_network_params를 이용해 업데이트
-    # Update the target network by using self.update_target_network_params
     def update_target_network(self):
         self.sess.run(self.update_target_network_params)
 
@@ -130,40 +103,29 @@ class CriticNetwork(object):
         self.tau = tau
         self.gamma = gamma
 
-        # critic network 생성
         with tf.variable_scope('critic_network') :
             self.inputs, self.action, self.out = self.create_critic_network()
 
         self.network_params = tf.trainable_variables()[num_actor_vars:]
 
-        # Target critic network 생성
         with tf.variable_scope('target_critic_network') :
             self.target_inputs, self.target_action, self.target_out = self.create_critic_network()
 
         self.target_network_params = tf.trainable_variables()[(len(self.network_params) + num_actor_vars):]
 
-        # Op for periodically updating target network with online network
-        # weights with regularization
         self.update_target_network_params = \
             [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) \
                                                   + tf.multiply(self.target_network_params[i], 1. - self.tau))
              for i in range(len(self.target_network_params))]
 
-        #target critic network에 y_i 값으로 제공될 placeholder
         self.predicted_q_value = tf.placeholder(tf.float32, [None, 1])
 
-        # loss 정의, 최적화
         self.loss = tf.reduce_mean(tf.square(self.predicted_q_value - self.out))
         self.optimize = tf.train.AdamOptimizer(
             self.learning_rate).minimize(self.loss)
 
-        # action에 대해서 신경망의 gradient를 구함
-        # 미니배치의 각 critic 출력의 (각 액션에 의해서 구해진)기울기를 합산
-        # 모든 출력은 자신이 나눠진 action을 제외한 모든 action에 대해 독립적임
         self.action_grads = tf.gradients(self.out, self.action)
 
-    # Critic network 정의
-    # Define the critic network
     def create_critic_network(self):
         inputs = tf.placeholder(shape=[None, self.s_dim], dtype=tf.float32)
         action = tf.placeholder(shape=[None, self.a_dim], dtype=tf.float32)
@@ -175,7 +137,6 @@ class CriticNetwork(object):
 
 
         w2 = tf.get_variable(name='criti_w2', shape=[400, 300], initializer=tf.random_uniform_initializer(-0.3, 0.3))
-        # action에 가중치를 곱해서 critic network에 더해준다. 경험적으로 좋은 결과를 이끌어냈다고 함.
 
 
         w2_a  = tf.get_variable(name='criticc_w2_action', shape=[self.a_dim, 300], initializer=tf.random_uniform_initializer(-0.3, 0.3))
