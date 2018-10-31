cfe7f359e6e06c54d10651c6b97925e35f506868
jinPrelude <jin.Prelude@gmail.com>
2018-10-31 22:27:31 +0900

    Add ddpg-untidy code0

--- pytorch_rl/algorithms.py
+++ pytorch_rl/algorithms.py
@@ -23,6 +23,168 @@ class Net(nn.Module):
         actions_value = self.out(x)
         return actions_value
 
+class DDPG_actor(nn.Module):
+    def __init__(self, state_space, action_space):
+        super(DDPG_actor, self).__init__()
+        self.fc1 = nn.Linear(state_space, 50)
+        self.fc1.weight.data.normal_(0, 0.1)   # initialization
+        self.fc2 = nn.Linear(50, 100)
+        self.fc2.weight.data.normal_(0, 0.1)  # initialization
+        self.fc3 = nn.Linear(100, 40)
+        self.fc3.weight.data.normal_(0, 0.1)  # initialization
+        self.out = nn.Linear(40, action_space)
+        self.out.weight.data.normal_(0, 0.1)   # initialization
+
+    def forward(self, x):
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = F.tanh(self.fc3(x))
+        actions_value = F.tanh(self.out(x))
+        return actions_value
+
+class DDPG_critic(nn.Module) :
+    def __init__(self, state_space, action_space):
+        super(DDPG_critic, self).__init__()
+        self.fc1 = nn.Linear(state_space+action_space, 50)
+        self.fc1.weight.data.normal_(0, 0.1)   # initialization
+        self.fc2 = nn.Linear(50, 100)
+        self.fc2.weight.data.normal_(0, 0.1)  # initialization
+        self.fc3 = nn.Linear(100, 40)
+        self.fc3.weight.data.normal_(0, 0.1)  # initialization
+        self.out = nn.Linear(40, 1)
+        self.out.weight.data.normal_(0, 0.1)   # initialization
+
+    def forward(self, x):
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        q_value = self.out(x)
+        return q_value
+
+
+
+class DDPG(object) :
+    def __init__(self, args, state_space, action_space):
+        self.args = args
+
+        self.epsilon = 1.
+        self.state_space = state_space
+        self.action_space = action_space
+
+        self.actor, self.target_actor = \
+            DDPG_actor(state_space, action_space), DDPG_actor(state_space, action_space)
+        self.critic, self.target_critic = \
+            DDPG_critic(state_space, action_space), DDPG_critic(state_space, action_space)
+
+        self.learn_step_counter = 0  # for target updating
+        self.memory_counter = 0  # for storing memory
+        self.memory = np.zeros((self.args.memory_capacity, self.state_space * 2 + 2))  # initialize memory
+        self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=self.args.lr)
+        self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=self.args.lr)
+        self.loss_func = nn.MSELoss()
+
+    def soft_update(self, target, source, tau):
+        for target_param, param in zip(target.parameters(), source.parameters()):
+
+            target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+    def choose_action(self, x):
+        x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        # a = torch.unsqueeze(torch.FloatTensor(a), 0)
+        # input only one sample
+        if np.random.uniform() > self.epsilon:  # greedy
+            actions_value = self.actor.forward(x)
+            action = actions_value.detach().numpy()[0]  # if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
+        else:  # random
+            action = np.random.rand(self.action_space)
+        return action
+
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % self.args.memory_capacity
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def learn(self):
+        # target parameter update
+        if self.learn_step_counter % self.args.target_replace_iter == 0:
+            self.target_actor.load_state_dict(self.actor.state_dict())
+        self.learn_step_counter += 1
+
+        # sample batch transitions
+        sample_index = np.random.choice(self.args.memory_capacity, self.args.batch_size)
+        b_memory = self.memory[sample_index, :]
+        b_s = torch.FloatTensor(b_memory[:, :self.state_space])
+        b_a = torch.LongTensor(b_memory[:, self.state_space:self.state_space + 1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, self.state_space + 1:self.state_space + 2])
+        b_s_ = torch.FloatTensor(b_memory[:, -self.state_space:])
+
+        b_target_action = self.target_actor(b_s_)
+        #y_critic_input = np.concatenate((b_s_, b_target_action), axis=1) # 디버깅으로 차원 확인
+        y_critic_input = torch.cat((b_s_, b_target_action), dim=1)
+        y = b_r + self.args.gamma * self.target_critic(y_critic_input)
+        critic_input = np.concatenate((b_s, b_a), axis=1)
+        #critic_input = torch.cat((b_s, b_a), dim=1)
+        critic_input = torch.unsqueeze(torch.FloatTensor(critic_input), 0)
+        q = self.critic(critic_input)
+
+        q_loss = self.loss_func(y, q)
+        self.critic_optim.zero_grad()
+        q_loss.backward()
+        self.critic_optim.step()
+
+        actor_q_input = torch.cat((b_s, self.actor(b_s)), dim=1)
+        actor_loss = -self.critic(actor_q_input).mean()
+        self.actor_optim.zero_grad()
+        actor_loss.backward()
+        self.actor_optim.step()
+
+        self.soft_update(self.target_actor, self.actor, self.args.tau)
+        self.soft_update(self.target_critic, self.critic, self.args.tau)
+
+
+    def pendulum_train_loop(self, env):
+
+        print('\nCollecting experience...')
+        for i_episode in range(self.args.num_episode):
+
+            if self.memory_counter > self.args.memory_capacity:  # decay epsilon at every episode
+                self.epsilon *= self.args.epsilon_decay
+                print('epsilon : ', self.epsilon)
+
+            s = env.reset()
+            ep_r = 0
+            while True:
+                env.render()
+                a = self.choose_action(s)
+                a *= 2.0
+                # take action
+                s_, r, done, info = env.step(a)
+
+                # modify the reward
+                # x, x_dot, theta, theta_dot = s_
+                # r1 = (env.env.x_threshold - abs(x)) / env.env.x_threshold - 0.8
+                # r2 = (env.env.theta_threshold_radians - abs(theta)) / env.env.theta_threshold_radians - 0.5
+                # r = r1 + r2
+
+                self.store_transition(s, a, r, s_)
+
+                ep_r += r
+                if self.memory_counter > self.args.memory_capacity:
+                    self.learn()
+                    if done:
+                        if i_episode % self.args.ep_print_iter == 0:
+                            print('Ep: ', i_episode,
+                                  '| Ep_r: ', round(ep_r, 2))
+                            if i_episode % self.args.model_save_iter == 0:
+                                torch.save(self.actor.state_dict(), './save_pendulum/model_%d' % (i_episode))
+
+                if done:
+                    break
+                s = s_
+
 
 class DQN(object):
     def __init__(self, args, state_space, action_space):
@@ -185,36 +347,3 @@ class DQN(object):
                 if done:
                     break
                 s = s_
-
-# class DDPG(object) :
-#     def __init__(self, args, state_space, action_space):
-#         self.args = args
-#
-#         self.epsilon = 1.
-#         self.state_space = state_space
-#         self.action_space = action_space
-#
-#         self.actor, self.target_actor =
-#         self.critic, self.target_critic =
-#         self.learn_step_counter = 0  # for target updating
-#         self.memory_counter = 0  # for storing memory
-#         self.memory = np.zeros((self.args.memory_capacity, self.state_space * 2 + 2))  # initialize memory
-#         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=self.args.lr)
-#         self.loss_func = nn.MSELoss()
-#
-#     def choose_action(self, x, a):
-#         x = torch.unsqueeze(torch.FloatTensor(x), 0)
-#         a = torch.unsqueeze(torch.FloatTensor(a), 0)
-#         # input only one sample
-#         if np.random.uniform() > self.epsilon:  # greedy
-#             actions_value = self.actor.forward(x)
-#             action = actions_value[0]  # if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
-#         else:  # random
-#             action = np.random.rand(0, self.action_space)
-#         return action
-#
-#     def learn(self, x):
-#         pass
-#
-#     def pandulum_train_loop(self, env):
-#         pass
